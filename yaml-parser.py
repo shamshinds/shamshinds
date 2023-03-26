1e80c729561f765b8b1e2ee9128b11bdd5479bf7
Dmitriy Shamshin <79089543+shamshinds@users.noreply.github.com>
2023-03-26 20:04:54 +0300

    Добавил YAML-парсер

---
+++ yaml-parser.py
@@ -0,0 +1,25 @@
+import yaml
+
+
+def paths(some_dict, path=()):
+    for key, value in some_dict.items():
+        if (isinstance(value, dict)):
+            key_path = path + (key + '.',)
+        else:
+            key_path = path + (key,) + (' : ',)
+            var = str(key_path)
+            for char in var:
+                if char in " (),'":
+                    var = var.replace(char, '')
+            print(var)
+            with open("D:/Yandex/vars.txt", "a", encoding="utf-8") as file:
+                file.write(var + ' ' + str(value) + '\n')
+        yield key_path
+        if hasattr(value, 'items'):
+            yield from paths(value, key_path)
+
+
+with open('D:/Yandex/key-vars.yaml', encoding='utf8') as f:
+    data = yaml.full_load(f)
+
+print(*paths(data['default']))
